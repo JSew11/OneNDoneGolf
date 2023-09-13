5d86f613d281fbcd04ee59f3ff1e67b165b61389
Joshua Seward <joshuaseward11@gmail.com>
2023-09-12 18:27:01 -0700

    setup settings module

--- backend/one_n_done_api/settings/base.py
+++ backend/one_n_done_api/settings/base.py
@@ -10,18 +10,17 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
-import os
-from pathlib import Path
+import os, os.path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = os.path.dirname(os.path.realpath('manage.py'))
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-!pl+4hp8i9q%t01r*3kjf5(v!-wy3wr^9@h8&r2d$6m@!j1d!d'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
---
+++ backend/one_n_done_api/settings/dev.py
@@ -0,0 +1,9 @@
+from .base import *
+
+DEBUG = True
+
+CORS_ORIGIN_ALLOW_ALL = True
+
+INSTALLED_APPS += [
+    'reset_migrations',
+]
