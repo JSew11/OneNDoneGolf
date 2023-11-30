07197ab7de6673fa98827cefb345c5193f4a3092
Joshua Seward <joshuaseward11@gmail.com>
2023-11-30 01:18:51 +0000

    refactored code in custom serializer create method

--- backend/golf_pickem/serializers/pick_serializer.py
+++ backend/golf_pickem/serializers/pick_serializer.py
@@ -1,6 +1,8 @@
-from rest_framework.serializers import ModelSerializer, PrimaryKeyRelatedField
+from rest_framework.serializers import ModelSerializer
 
+from core.models.user import User
 from ..models.pick import Pick
+from ..models.tournament_golfer import TournamentGolfer
 
 class PickSerializer(ModelSerializer):
     """Serializer for the pick model.
@@ -14,6 +16,6 @@ class PickSerializer(ModelSerializer):
     def create(self, validated_data):
         """Overwritten create method to work with the custom PickManager class.
         """
-        user_id = validated_data['user'].id
-        tournament_golfer_id = validated_data['tournament_golfer'].id
-        return Pick.objects.create(user_id=user_id, tournament_golfer_id=tournament_golfer_id)
+        user: User = validated_data['user']
+        tournament_golfer: TournamentGolfer = validated_data['tournament_golfer']
+        return Pick.objects.create(user_id=user.id, tournament_golfer_id=tournament_golfer.id)
