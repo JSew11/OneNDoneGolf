d8b0a5c7988e972a36ea4730b0e5a849a715633a
Joshua Seward <joshuaseward11@gmail.com>
2024-01-30 00:07:50 +0000

    made pick and season api files

---
+++ client-web/src/api/pick.jsx
@@ -0,0 +1,41 @@
+import { privateAxios } from 'src/api/axios.jsx';
+
+const PICK_API_BASE_URL = 'picks/';
+
+const list = async () => {
+    return await privateAxios.get(PICK_API_BASE_URL);
+};
+
+const create = async (seasonId, tournamentId, golferId) => {
+    const newPickData = {
+        season_id: seasonId,
+        tournament_id: tournamentId,
+        golfer_id: golferId,
+    };
+    return await privateAxios.post(PICK_API_BASE_URL, newPickData);
+};
+
+const retrieve = async (pickId) => {
+    return await privateAxios.get(PICK_API_BASE_URL + pickId + '/');
+};
+
+const changeGolfer = async (pickId, newGolferId) => {
+    const updatedPickData = {
+        golfer_id: newGolferId
+    };
+    return await privateAxios.patch(PICK_API_BASE_URL + pickId + '/', updatedPickData);
+};
+
+const destroy = async (pickId) => {
+    return await privateAxios.delete(PICK_API_BASE_URL + pickId);
+}
+
+const SeasonsApi = {
+    list,
+    create,
+    retrieve,
+    changeGolfer,
+    destroy
+};
+
+export default SeasonsApi;
---
+++ client-web/src/api/season.jsx
@@ -0,0 +1,33 @@
+import { privateAxios } from 'src/api/axios.jsx';
+
+const SEASON_API_BASE_URL = 'seasons/';
+
+const list = async () => {
+    return await privateAxios.get(SEASON_API_BASE_URL);
+};
+
+const create = async (newSeasonData) => {
+    return await privateAxios.post(SEASON_API_BASE_URL, newSeasonData);
+};
+
+const retrieve = async (seasonId) => {
+    return await privateAxios.get(SEASON_API_BASE_URL + seasonId + '/');
+};
+
+const partialUpdate = async (seasonId, updatedSeasonData) => {
+    return await privateAxios.patch(SEASON_API_BASE_URL + seasonId + '/', updatedSeasonData);
+};
+
+const destroy = async (seasonId) => {
+    return await privateAxios.delete(SEASON_API_BASE_URL + seasonId);
+}
+
+const SeasonsApi = {
+    list,
+    create,
+    retrieve,
+    partialUpdate,
+    destroy
+};
+
+export default SeasonsApi;
