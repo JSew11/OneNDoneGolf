8f54118dca7572f5993f077973ed688bc348af46
Joshua Seward <joshuaseward11@gmail.com>
2025-04-08 01:02:55 +0000

    Got the full standings table started and made select for switching between tables

--- client-web/src/assets/components/fullTournamentLeaderboard.jsx
+++ client-web/src/assets/components/fullTournamentLeaderboard.jsx
@@ -8,17 +8,22 @@ import {
   StyledTitleCell
 } from 'src/assets/components/styledTable/tableCells';
 
-const PickedGolfersLeaderboard = ({ seasonId, tournamentId }) => {
+const FullTournamentLeaderboard = ({ seasonId, tournamentId }) => {
   return (
     <Table stickyHeader size='small' className='my-0 pb-3'>
       <TableHead>
         <StyledTableRow key='header'>
+          <StyledTableCell align='center'>Place</StyledTableCell>
           <StyledTableCell align='center'>Golfer</StyledTableCell>
-          <StyledTableCell align='center'>Current Place</StyledTableCell>
           <StyledTableCell align='center'>Overall Score</StyledTableCell>
-          <StyledTableCell align='center'>Thru</StyledTableCell>
           <StyledTableCell align='center'>Today's Score</StyledTableCell>
-          <StyledTableCell align='center'>Times Picked</StyledTableCell>
+          <StyledTableCell align='center'>Thru</StyledTableCell>
+          <StyledTableCell align='center'>R1</StyledTableCell>
+          <StyledTableCell align='center'>R2</StyledTableCell>
+          <StyledTableCell align='center'>R3</StyledTableCell>
+          <StyledTableCell align='center'>R4</StyledTableCell>
+          <StyledTableCell align='center'>Total</StyledTableCell>
+          <StyledTableCell align='center'>Winnings</StyledTableCell>
         </StyledTableRow>
       </TableHead>
       <TableBody>
@@ -30,4 +35,4 @@ const PickedGolfersLeaderboard = ({ seasonId, tournamentId }) => {
   );
 }
 
-export default PickedGolfersLeaderboard;
+export default FullTournamentLeaderboard;
