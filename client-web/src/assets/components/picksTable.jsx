e09c11e0b8e23d6bf1efc075e12ff66b2e37b7a6
Joshua Seward <joshuaseward11@gmail.com>
2025-11-17 22:32:22 +0000

    Added column headers row and progress spinner

--- client-web/src/assets/components/picksTable.jsx
+++ client-web/src/assets/components/picksTable.jsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from 'react';
 import Table from '@mui/material/Table';
 import TableHead from '@mui/material/TableHead';
 import TableBody from '@mui/material/TableBody';
+import CircularProgress from '@mui/material/CircularProgress';
 
 import StyledTableRow from 'src/assets/components/styledTable/row';
 import {
@@ -10,11 +11,14 @@ import {
 } from 'src/assets/components/styledTable/tableCells';
 
 const PicksTable = ({ seasonId }) => {
+
+  const [tableData, setTableData] = useState([]);
+
   return (
     <Table stickyHeader size='small' className='my-0 pb-3'>
       <TableHead>
-        <StyledTableRow>
-          <StyledTitleCell>{
+        <StyledTableRow key='title'>
+          <StyledTitleCell colSpan={4}>{
               seasonId ?
                 'TODO - dropdown to select user'
               :
@@ -22,10 +26,25 @@ const PicksTable = ({ seasonId }) => {
             }
           </StyledTitleCell>
         </StyledTableRow>
-        {/* TODO - Add filter, and header rows */}
+        <StyledTableRow key='header'>
+          <StyledTableCell align='center'>Tournament</StyledTableCell>
+          <StyledTableCell align='center'>Golfer</StyledTableCell>
+          <StyledTableCell align='center'>Place</StyledTableCell>
+          <StyledTableCell align='center'>Prize Money</StyledTableCell>
+        </StyledTableRow>
       </TableHead>
       <TableBody>
-        {/* TODO - Add data rows */}
+        {
+          seasonId && tableData.length > 0 ?
+            // TODO - render table data
+            ''
+          :
+            <StyledTableRow>
+              <StyledTableCell align='center' colSpan={11}>
+                <CircularProgress className='my-4' size='50px'/>
+              </StyledTableCell>
+            </StyledTableRow>
+        }
       </TableBody>
     </Table>
   );
