ab13c6e0b40961369af2109329a0f5cc3ca853f8
JSew11 <joshuaseward11@gmail.com>
2026-07-13 16:23:41 -0700

    Got the services starting but not connecting

--- backend/one_n_done_api/celery.py
+++ backend/one_n_done_api/celery.py
@@ -4,9 +4,17 @@ from celery import Celery
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'one_n_done_api.settings')
 
+RABBITMQ = {
+    "PROTOCOL": "amqp", # change to "amqps" on prod
+    "HOST": os.getenv("RABBITMQ_HOST", "localhost"),
+    "PORT": os.getenv("RABBITMQ_PORT", 5672),
+    "USER": os.getenv("RABBITMQ_USER", "guest"),
+    "PASSWORD": os.getenv("RABBITMQ_PASSWORD", "guest"),
+}
+
 app = Celery(
     'one_n_done_api',
-    broker=os.environ.get('CELERY_BROKER')
+    broker=f"{RABBITMQ['PROTOCOL']}://{RABBITMQ['USER']}:{RABBITMQ['PASSWORD']}@{RABBITMQ['HOST']}:{RABBITMQ['PORT']}"
 )
 
 app.config_from_object('django.conf:settings', namespace='CELERY')
