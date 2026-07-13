f351db6700ff949c1199fea90423db3e6af6e8bf
JSew11 <joshuaseward11@gmail.com>
2026-07-13 15:13:40 -0700

    started working on adding celery worker container to docker compose environment

--- backend/one_n_done_api/settings/base.py
+++ backend/one_n_done_api/settings/base.py
@@ -174,3 +174,13 @@ REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 10
 }
+
+RABBITMQ = {
+    "PROTOCOL": "amqps",
+    "HOST": os.getenv("RABBITMQ_HOST", "localhost"),
+    "PORT": os.getenv("RABBITMQ_PORT", 5672),
+    "USER": os.getenv("RABBITMQ_USER", "guest"),
+    "PASSWORD": os.getenv("RABBITMQ_PASSWORD", "guest"),
+}
+
+CELERY_BROKER_URL = f"{RABBITMQ['PROTOCOL']}://{RABBITMQ['USER']}:{RABBITMQ['PASSWORD']}@{RABBITMQ['HOST']}:{RABBITMQ['PORT']}"
--- backend/one_n_done_api/settings/dev.py
+++ backend/one_n_done_api/settings/dev.py
@@ -7,3 +7,11 @@ CORS_ORIGIN_ALLOW_ALL = True
 INSTALLED_APPS += [
     'reset_migrations',
 ]
+
+RABBITMQ = {
+    "PROTOCOL": "amqp",
+    "HOST": os.getenv("RABBITMQ_HOST", "localhost"),
+    "PORT": os.getenv("RABBITMQ_PORT", 5672),
+    "USER": os.getenv("RABBITMQ_USER", "guest"),
+    "PASSWORD": os.getenv("RABBITMQ_PASSWORD", "guest"),
+}
