2de0119069a91e6a6178c3ea214d591c99284971
JSew11 <joshuaseward11@gmail.com>
2026-07-20 17:14:11 -0700

    Got celery connecting correctly

--- backend/one_n_done_api/celery.py
+++ backend/one_n_done_api/celery.py
@@ -2,20 +2,10 @@ import os
 
 from celery import Celery
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'one_n_done_api.settings')
+# todo - change this to use 'one_n_done_api.settings.base' when deploying
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'one_n_done_api.settings.dev')
 
-RABBITMQ = {
-    "PROTOCOL": "amqp", # change to "amqps" on prod
-    "HOST": os.getenv("RABBITMQ_HOST", "localhost"),
-    "PORT": os.getenv("RABBITMQ_PORT", 5672),
-    "USER": os.getenv("RABBITMQ_USER", "guest"),
-    "PASSWORD": os.getenv("RABBITMQ_PASSWORD", "guest"),
-}
-
-app = Celery(
-    'one_n_done_api',
-    broker=f"{RABBITMQ['PROTOCOL']}://{RABBITMQ['USER']}:{RABBITMQ['PASSWORD']}@{RABBITMQ['HOST']}:{RABBITMQ['PORT']}"
-)
+app = Celery('one_n_done_api')
 
 app.config_from_object('django.conf:settings', namespace='CELERY')
 
